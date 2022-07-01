d3d91aa213b98d27ecc4d4a26fe9d50348c97f2a
JimmyYang20 <yangjin39@huawei.com>
2022-07-01 10:12:29 +0800

    Add new metric: data transfer count ratio
    Signed-off-by: JimmyYang20 <yangjin39@huawei.com>

--- core/testcasecontroller/metrics/metrics.py
+++ core/testcasecontroller/metrics/metrics.py
@@ -12,36 +12,65 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""Base Metrics"""
+
 import sys
-import numpy as np
 
 from sedna.common.class_factory import ClassFactory, ClassType
 
+from core.common.constant import SystemMetricKind
 from core.common.utils import load_module
 
-def smape(y_true, y_pred):
-    y_true = np.array(y_true).reshape(-1, 1)
-    y_pred = np.array(y_pred).reshape(-1, 1)
-    return np.mean(np.nan_to_num(np.abs(y_true - y_pred) / (np.abs(y_pred) + np.abs(y_true))))
+
+def data_transfer_count_ratio(system_metric_info: dict):
+    """
+    compute data transfer count ratio
+
+    Parameters
+    ----------
+    system_metric_info: dict
+        information needed to compute system metrics.
+
+    Returns
+    -------
+    float
+        e.g.: 0.92
+
+    """
+
+    info = system_metric_info.get(SystemMetricKind.DATA_TRANSFER_COUNT_RATIO.value)
+    inference_num = 0
+    transfer_num = 0
+    for inference_data, transfer_data in info:
+        inference_num += len(inference_data)
+        transfer_num += len(transfer_data)
+    return float(transfer_num) / inference_num
 
 
-def max_error_rate(y_true, y_pred):
-    return max(np.nan_to_num(np.abs(y_true - y_pred) / (np.abs(y_pred) + np.abs(y_true))))
+def get_metric_func(metric_dict: dict):
+    """
+    get metric func by metric info
 
+    Parameters:
+    ----------
+    metric_dict: dict
+        metric info, e.g.: {"name": "f1_score", "url": "/metrics/f1_score.py"}
 
-def get_metric_func(metric_name: str = None, metric_dict: dict = None):
-    """ get metric func """
-    if isinstance(metric_name, str):
-        return getattr(sys.modules[__name__], metric_name)
-    elif isinstance(metric_dict, dict):
-        name = metric_dict.get("name")
-        url = metric_dict.get("url")
-        if url:
-            load_module(url)
-            try:
-                metric_func = ClassFactory.get_cls(type_name=ClassType.GENERAL, t_cls_name=name)
-            except Exception as err:
-                raise Exception(f"get metric func(url={url}) failed, error: {err}.")
-            return metric_func
-        else:
-            return getattr(sys.modules[__name__], name)
+    Returns:
+    -------
+    name: string
+        metric name
+    metric_func: function
+    """
+
+    name = metric_dict.get("name")
+    url = metric_dict.get("url")
+    if url:
+        load_module(url)
+        try:
+            metric_func = ClassFactory.get_cls(type_name=ClassType.GENERAL, t_cls_name=name)
+            return name, metric_func
+        except Exception as err:
+            raise Exception(f"get metric func(url={url}) failed, error: {err}.") from err
+
+    return name, getattr(sys.modules[__name__], name)
