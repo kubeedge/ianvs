9b5354646c264babea22cd435bd82266e73ec58d
Siqi Luo <1587295470@qq.com>
2022-11-23 10:37:31 +0800

    Fix pylint errors
    Signed-off-by: Siqi Luo <1587295470@qq.com>

--- examples/cityscapes/singletask_learning_bench/testalgorithms/rfnet/basemodel.py
+++ examples/cityscapes/singletask_learning_bench/testalgorithms/rfnet/basemodel.py
@@ -15,22 +15,16 @@
 from __future__ import absolute_import, division, print_function
 
 import os
-import tempfile
-import time
 import zipfile
-import cv2
 import logging
 
 import numpy as np
 from PIL import Image
-import argparse
-from tqdm import tqdm
 import torch
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from sedna.common.class_factory import ClassType, ClassFactory
 from sedna.common.config import Context
-from sedna.datasources import TxtDataParse
 from sedna.common.file_ops import FileOps
 from RFNet.dataloaders import make_data_loader
 from RFNet.dataloaders import custom_transforms as tr
@@ -48,13 +42,14 @@ __all__ = ["BaseModel"]
 
 os.environ['BACKEND_TYPE'] = 'TORCH'
 
+
 @ClassFactory.register(ClassType.GENERAL, alias="RFNet")
 class BaseModel:
 
     def __init__(self, **kwargs):
         self.train_cfgs = train_cfgs
         self.valid_cfgs = valid_cfgs
-        
+
         self.train_cfgs.lr = kwargs.get("learning_rate", 0.0001)
         self.train_cfgs.momentum = kwargs.get("momentum", 0.9)
         self.train_cfgs.epochs = kwargs.get("epochs", 2)
@@ -63,28 +58,30 @@ class BaseModel:
         self.train_cfgs.depth = True
 
         os.environ["MODEL_NAME"] = "model_best_mapi_only.pth"
-        
+
         self.train_cfgs.gpu_ids = ['cuda:0']
         self.valid_cfgs.gpu_ids = ['cuda:0']
         self.valid_cfgs.depth = True
-        
+
         self.validator = Validator(self.valid_cfgs)
         self.trainer = Trainer(self.train_cfgs)
         self.trainer.saver = Saver(self.train_cfgs)
-        self.checkpoint_path = self.load(Context.get_parameters("base_model_url"))
+        self.checkpoint_path = self.load(
+            Context.get_parameters("base_model_url"))
 
     def train(self, train_data, valid_data=None, **kwargs):
         if train_data is None or train_data.x is None or train_data.y is None:
             raise Exception("Train data is None.")
 
         self.trainer.train_loader, self.trainer.val_loader, _, _ = \
-            make_data_loader(self.trainer.args, train_data=train_data, valid_data=valid_data, **kwargs)
+            make_data_loader(
+                self.trainer.args, train_data=train_data, valid_data=valid_data, **kwargs)
 
         # Define lr scheduler
         self.trainer.scheduler = LR_Scheduler(self.trainer.args.lr_scheduler, self.trainer.args.lr,
                                               self.trainer.args.epochs, len(self.trainer.train_loader))
         print("Total epoches:", self.trainer.args.epochs)
-        
+
         for epoch in range(self.trainer.args.start_epoch, self.trainer.args.epochs):
             if epoch == 0 and self.trainer.val_loader:
                 self.trainer.validation(epoch)
@@ -101,15 +98,6 @@ class BaseModel:
                     'optimizer': self.trainer.optimizer.state_dict(),
                     'best_pred': self.trainer.best_pred,
                 }, is_best)
-                
-            # if not self.trainer.args.no_val and \
-            #         epoch % self.train_args.eval_interval == (self.train_args.eval_interval - 1) \
-            #         and self.trainer.val_loader:
-            #     self.trainer.validation(epoch)
-            # self.checkpoint_path = os.path.join(self.temp_dir, '{}_'.format(
-            #                 cfgs.DATASET_NAME) + str(_global_step) + "_" + str(time.time()) + '_model.ckpt')
-            #             saver.save(sess, self.checkpoint_path)
-            #             print('Weights have been saved to {}.'.format(self.checkpoint_path))
 
         self.trainer.writer.close()
 
@@ -120,7 +108,8 @@ class BaseModel:
             raise Exception("model path is None.")
 
         model_dir, model_name = os.path.split(self.checkpoint_path)
-        models = [model for model in os.listdir(model_dir) if model_name in model]
+        models = [model for model in os.listdir(
+            model_dir) if model_name in model]
 
         if os.path.splitext(model_path)[-1] != ".zip":
             model_path = os.path.join(model_path, "model.zip")
@@ -131,7 +120,8 @@ class BaseModel:
         with zipfile.ZipFile(model_path, "w") as f:
             for model_file in models:
                 model_file_path = os.path.join(model_dir, model_file)
-                f.write(model_file_path, model_file, compress_type=zipfile.ZIP_DEFLATED)
+                f.write(model_file_path, model_file,
+                        compress_type=zipfile.ZIP_DEFLATED)
 
         return model_path
 
@@ -142,16 +132,19 @@ class BaseModel:
         if type(data) is np.ndarray:
             data = data.tolist()
 
-        self.validator.test_loader = DataLoader(data, batch_size=self.validator.args.test_batch_size, shuffle=False, pin_memory=True)
+        self.validator.test_loader = DataLoader(
+            data, batch_size=self.validator.args.test_batch_size, shuffle=False, pin_memory=True)
         return self.validator.validate()
 
     def load(self, model_url):
         model_url = '/home/wxc/dev/ianvs/models/model_best_mapi_only.pth'
         if FileOps.exists(model_url):
-            self.validator.new_state_dict = torch.load(model_url, map_location=torch.device("cpu"))
+            self.validator.new_state_dict = torch.load(
+                model_url, map_location=torch.device("cpu"))
         else:
             raise Exception("model url does not exist.")
-        self.validator.model = load_my_state_dict(self.validator.model, self.validator.new_state_dict['state_dict'])
+        self.validator.model = load_my_state_dict(
+            self.validator.model, self.validator.new_state_dict['state_dict'])
         return model_url
 
     def _preprocess(self, image_urls):
@@ -164,7 +157,8 @@ class BaseModel:
             composed_transforms = transforms.Compose([
                 # tr.CropBlackArea(),
                 # tr.FixedResize(size=self.args.crop_size),
-                tr.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+                tr.Normalize(mean=(0.485, 0.456, 0.406),
+                             std=(0.229, 0.224, 0.225)),
                 tr.ToTensor()])
 
             transformed_images.append((composed_transforms(sample), img_path))
