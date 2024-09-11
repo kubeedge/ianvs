6034ac2a0028bb8de5e38a7b2b0ba6cc5714233b
IcyFeather <mengzhuo.happy@gmail.com>
2024-09-11 22:22:02 +0800

    update llm government benchmark implementation
    Signed-off-by: IcyFeather <mengzhuo.happy@gmail.com>

--- examples/government/singletask_learning_bench/objective/testenv/acc.py
+++ examples/government/singletask_learning_bench/objective/testenv/acc.py
@@ -34,8 +34,6 @@ def get_last_letter(input_string):
 def acc(y_true, y_pred):
     y_pred = [get_last_letter(pred) for pred in y_pred]
     y_true = [get_last_letter(pred) for pred in y_true]
-    # print(y_true)
-    # print(y_pred)
         
     # 使用列表推导来比较两个列表中的元素是否相同
     same_elements = [y_pred[i] == y_true[i] for i in range(len(y_pred))]
