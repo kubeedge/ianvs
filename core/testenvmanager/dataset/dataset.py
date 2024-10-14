917d8df526bb7f9cb0489e3d5aa707b7db9d182c
yexiaochuan <yxc2020@foxmail.com>
2024-10-14 16:44:13 +0800

    fix pylint 3.9 too-many-positional-arguments
    Signed-off-by: yexiaochuan <yxc2020@foxmail.com>

--- core/testenvmanager/dataset/dataset.py
+++ core/testenvmanager/dataset/dataset.py
@@ -116,6 +116,7 @@ class Dataset:
         self.test_url = self._process_index_file(self.test_url)
 
     # pylint: disable=too-many-arguments
+    # pylint: disable=too-many-positional-arguments
     def split_dataset(self, dataset_url, dataset_format, ratio, method="default",
                       dataset_types=None, output_dir=None, times=1):
         """
@@ -202,14 +203,16 @@ class Dataset:
             data = pd.read_csv(data_file)
 
         return data
-
+    
+    # pylint: disable=too-many-positional-arguments, R0913
     def _get_dataset_file(self, data, output_dir, dataset_type, index, dataset_format):
         data_file = self._get_file_url(output_dir, dataset_type, index, dataset_format)
 
         self._write_data_file(data, data_file, dataset_format)
 
         return data_file
-
+    
+    # pylint: disable=too-many-positional-arguments, R0913
     def _splitting_more_times(self, data_file, data_format, ratio,
                               data_types=None, output_dir=None, times=1):
         if not data_types:
@@ -243,6 +246,7 @@ class Dataset:
 
         return data_files
 
+    # pylint: disable=too-many-positional-arguments, R0913
     def _fwt_splitting(self, data_file, data_format, ratio,
                               data_types=None, output_dir=None, times=1):
         if not data_types:
@@ -282,6 +286,7 @@ class Dataset:
         return data_files
 
     # add new splitting method for semantic segmentation
+    # pylint: disable=too-many-positional-arguments, R0913
     def _city_splitting(self, data_file, data_format, ratio,
                               data_types=None, output_dir=None, times=1):
         if not data_types:
@@ -326,6 +331,7 @@ class Dataset:
 
         return data_files
 
+    # pylint: disable=too-many-positional-arguments, R0913
     def _hard_example_splitting(self, data_file, data_format, ratio,
                               data_types=None, output_dir=None, times=1):
         if not data_types:
@@ -365,6 +371,7 @@ class Dataset:
         return data_files
 
     @classmethod
+    # pylint: disable=too-many-positional-arguments, R0913
     def load_data(cls, file: str, data_type: str, label=None, use_raw=False, feature_process=None):
         """
         load data
