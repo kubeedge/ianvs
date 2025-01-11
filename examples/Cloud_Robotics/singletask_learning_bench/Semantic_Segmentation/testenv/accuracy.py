e75b0dc96ab0f3f2a41daef604ed801f8949cb6f
Aryan Yadav <141573833+aryan0931@users.noreply.github.com>
2025-01-11 15:11:07 +0530

    Update accuracy.py

--- examples/Cloud_Robotics/singletask_learning_bench/Semantic_Segmentation/testenv/accuracy.py
+++ examples/Cloud_Robotics/singletask_learning_bench/Semantic_Segmentation/testenv/accuracy.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import logging
 from tqdm import tqdm
 
 from sedna.common.class_factory import ClassType, ClassFactory
@@ -20,15 +21,15 @@ from RFNet.dataloaders import make_data_loader
 from RFNet.utils.metrics import Evaluator
 from RFNet.utils.args import ValArgs
 
-__all__ = ('accuracy')
+__all__ = ('accuracy', 'compute_cpa', 'compute_miou', 'compute_fwiou')
 
+logger = logging.getLogger(__name__)
 
 @ClassFactory.register(ClassType.GENERAL, alias="accuracy")
 def accuracy(y_true, y_pred, **kwargs):
     args = ValArgs()
     _, _, test_loader, num_class = make_data_loader(args, test_data=y_true)
     evaluator = Evaluator(num_class)
-    #print(y_true)
     tbar = tqdm(test_loader, desc='\r')
     for i, (sample, img_path) in enumerate(tbar):
         if args.depth:
@@ -42,17 +43,26 @@ def accuracy(y_true, y_pred, **kwargs):
 
         target[target > evaluator.num_class-1] = 255
         target = target.cpu().numpy()
-        #if i% 20 == 0:
-        #    print(img_path)
-        #    print(image, target, y_pred[i])
-        # Add batch sample into evaluator
         evaluator.add_batch(target, y_pred[i])
 
-    # Test during the training
-    # Acc = evaluator.Pixel_Accuracy()
-    CPA = evaluator.Pixel_Accuracy_Class()
-    mIoU = evaluator.Mean_Intersection_over_Union()
-    FWIoU = evaluator.Frequency_Weighted_Intersection_over_Union()
+    CPA = compute_cpa(evaluator)
+    mIoU = compute_miou(evaluator)
+    FWIoU = compute_fwiou(evaluator)
 
-    print("CPA:{}, mIoU:{}, fwIoU: {}".format(CPA, mIoU, FWIoU))
+    logger.info("CPA:{}, mIoU:{}, fwIoU: {}".format(CPA, mIoU, FWIoU))
     return mIoU
+
+def compute_cpa(evaluator):
+    CPA = evaluator.Pixel_Accuracy_Class()
+    logger.info(f"CPA: {CPA}")
+    return CPA
+
+def compute_miou(evaluator):
+    mIoU = evaluator.Mean_Intersection_over_Union()
+    logger.info(f"mIoU: {mIoU}")
+    return mIoU
+
+def compute_fwiou(evaluator):
+    FWIoU = evaluator.Frequency_Weighted_Intersection_over_Union()
+    logger.info(f"FWIoU: {FWIoU}")
+    return FWIoU
