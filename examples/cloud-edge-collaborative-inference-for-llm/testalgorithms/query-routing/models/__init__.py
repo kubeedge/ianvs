99bc2b6520ff6dd124f23bac28966cbdb022d4d8
FuryMartin <furymartin9910@outlook.com>
2025-01-16 03:32:30 +0800

    feat: add EAGLE for MMLU-zero-shot
    Signed-off-by: FuryMartin <furymartin9910@outlook.com>
    Signed-off-by: chengguoliang <2837224840@qq.com>

--- examples/cloud-edge-collaborative-inference-for-llm/testalgorithms/query-routing/models/__init__.py
+++ examples/cloud-edge-collaborative-inference-for-llm/testalgorithms/query-routing/models/__init__.py
@@ -2,3 +2,5 @@ from .api_llm import APIBasedLLM
 from .huggingface_llm import HuggingfaceLLM
 from .vllm_llm import VllmLLM
 from .base_llm import BaseLLM
+from .speculative_decoding_models.eagle_llm import EagleSpecDecModel
+from .speculative_decoding_models.lade_llm import LadeSpecDecLLM
