4519213d4db6b393c252130fbdbbd5156b88ad58
ajmani-x <aryanajmani7@gmail.com>
2026-07-30 20:08:26 +0530

    fix(examples/robot-cityscapes-synthia): stop hardcoding /tmp for ERFNet checkpoints
    Saver.__init__ hardcoded /tmp as the checkpoint root, writing every
    training round's weights outside the Ianvs-managed workspace. This
    loses checkpoints on reboot/tmp cleanup, breaks on Windows (no /tmp),
    and lets stale experiment_N directories from a previous run corrupt
    the run-id numbering on the next run.
    
    Use the OUTPUT_URL env var Ianvs sets for the workspace, falling back
    to tempfile.mkdtemp() when it's not set.
    
    Related to #467
    
    Signed-off-by: ajmani-x <aryanajmani7@gmail.com>

--- examples/robot-cityscapes-synthia/lifelong_learning_bench/semantic-segmentation/testalgorithms/erfnet/ERFNet/utils/saver.py
+++ examples/robot-cityscapes-synthia/lifelong_learning_bench/semantic-segmentation/testalgorithms/erfnet/ERFNet/utils/saver.py
@@ -10,7 +10,8 @@ class Saver(object):
 
     def __init__(self, args):
         self.args = args
-        self.directory = os.path.join('/tmp', args.dataset, args.checkname)
+        output_url = os.environ.get("OUTPUT_URL", tempfile.mkdtemp())
+        self.directory = os.path.join(output_url, args.dataset, args.checkname)
         self.runs = sorted(glob.glob(os.path.join(self.directory, 'experiment_*')))
         run_id = int(self.runs[-1].split('_')[-1]) + 1 if self.runs else 0
 
